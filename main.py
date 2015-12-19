9615d5994610cbf0c38967293cccc9e40ab8e157
Ozzyboshi <gun101@email.it>
2015-12-19 07:50:24 +0000

    added template selection and new web template for load balancing

--- main.py
+++ main.py
@@ -52,16 +52,23 @@ def get_services():
         endpoints["backends"].append(dict(name=container, addr=addr))
         port='80'
         if "HOST_SERVE_PORT" in os.environ:
+            print 'entro'
             port=os.environ["HOST_SERVE_PORT"]
         endpoints["port"] = port
     return services
 
-def generate_config(services):
-    template = env.get_template('haproxy.cfg.tmpl')
+def generate_config(services,templatefile):
+    template = env.get_template(templatefile)
     with open("/etc/haproxy.cfg", "w") as f:
         f.write(template.render(services=services))
 
 if __name__ == "__main__":
+
+    # get template from arguments if set
+    template='haproxy.cfg.tmpl';
+    if len(sys.argv)>1:
+        template=sys.argv[1]
+
     current_services = {}
     while True:
         try:
@@ -72,7 +79,7 @@ if __name__ == "__main__":
                 continue
 
             print "config changed. reload haproxy"
-            generate_config(services)
+            generate_config(services,template)
             ret = call(["./reload-haproxy.sh"])
             if ret != 0:
                 print "reloading haproxy returned: ", ret
